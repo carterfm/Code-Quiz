54afec630bff5aa91c28de223c80a6073c4495c9
carterfm <carterf.morfitt@gmail.com>
2022-01-02 17:07:28 -0800

    Added baseline CSS formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,11 @@
-//Test code for getting a feel for web API stuff
-/*var testElement = document.querySelector(".test");
-console.log("testElement: ")
-console.log(testElement);
-var buttonElement = document.querySelector("#reveal");
-console.log("buttonElement: ");
-console.log(buttonElement);
-
-testElement.setAttribute("style", "display: none");
-var revealed = false;
-
-buttonElement.addEventListener("click", function() {
-    if(!revealed){
-        console.log("Revealed is false; revealing text")
-        console.log("Clicked the button");
-        testElement.setAttribute("style", "color: purple; font-size: 80px; margin: 0px");
-        buttonElement.textContent = "Press to hide h2";
-        revealed = true;
-        console.log("Revealed set to " + revealed);
-    } else {
-        console.log("Revealed is true; hiding text")
-        console.log("Clicked the button");
-        testElement.setAttribute("style", "display: none");
-        buttonElement.textContent = "Press to reveal hidden h2";
-        revealed = false;
-        console.log("Revealed set to " + revealed);
-    }
-    
-})*/
-
 //Defining variables to keep track of html elements I want to dynamically 
 //hide and reveal using web API shenanigans
+/*var headerEl = querySelector("header");
+console.log(headerEl);
+var welcomeEl = querySelector("#welcome-screen");
+var welcome*/
+
+
+
+//So, we need to keep track of which answer is true and which one is false, huh
+//How can we do that?
